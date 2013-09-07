e2046f228b2c09fa32e621b373a524c514a43345
Zigmund Ozea <ziggy@dissectcyber.com>
2013-09-07 15:59:16 -0400

    Now cleans up ok files older than 48 hours for SCRIPTNAME_NOARGS.

--- process-locking-header.sh
+++ process-locking-header.sh
@@ -1,24 +1,42 @@
 # Configure your preferred lock file dir:
 LOCKSDIR="/var/lock" 
+# Remove ok files created before RM_OLD_OK_TS for SCRIPTNAME_NOARGS
+RM_OLD_OK_TS="48 hours ago"	
 #
 # Nothing below here needs configuration
 #
+
+SCRIPTNAME_NOARGS=`echo "$0"|rev|cut -d'/' -f1|rev`
+
 SCRIPTNAME=`echo "${0}"|rev|cut -d"/" -f1|rev|sed -e "s/\$/$1.$2.$3/"'; s/[^[:alnum:],.]//g; s/\.\.*/./g; s/.$//'`
 
-if [ -e "${LOCKSDIR}/$SCRIPTNAME.ok" ];then
-        mv ${LOCKSDIR}/$SCRIPTNAME.ok ${LOCKSDIR}/$SCRIPTNAME.running
-        touch ${LOCKSDIR}/$SCRIPTNAME.running
+if [ -e "${LOCKSDIR}/${SCRIPTNAME}.ok" ];then
+        mv "${LOCKSDIR}/${SCRIPTNAME}.ok" "${LOCKSDIR}/${SCRIPTNAME}.running"
+        touch "${LOCKSDIR}/${SCRIPTNAME}.running"
         /usr/bin/logger "Starting $SCRIPTNAME" 
 else   
-        if [ -e "${LOCKSDIR}/$SCRIPTNAME.running" ];then
+        if [ -e "${LOCKSDIR}/${SCRIPTNAME}.running" ];then
                 echo "Exiting because process is already running"
                 exit
         else   
-                /usr/bin/logger "Creating $SCRIPTNAME.ok"
-                touch ${LOCKSDIR}/$SCRIPTNAME.ok
+                /usr/bin/logger "Creating ${SCRIPTNAME}.ok"
+                touch "${LOCKSDIR}/${SCRIPTNAME}.ok"
                 echo "Created the ok file - must be first time running."
-        	mv ${LOCKSDIR}/$SCRIPTNAME.ok ${LOCKSDIR}/$SCRIPTNAME.running
-        	touch ${LOCKSDIR}/$SCRIPTNAME.running
-        	/usr/bin/logger "Starting $SCRIPTNAME" 
+        	mv "${LOCKSDIR}/$SCRIPTNAME.ok" "${LOCKSDIR}/${SCRIPTNAME}.running"
+        	touch "${LOCKSDIR}/${SCRIPTNAME}.running"
+        	/usr/bin/logger "Starting ${SCRIPTNAME}" 
         fi
+fi
+
+# Clean up old ok files for files begining with SCRIPTNAME_NOARGS and ending with .ok
+OK_FILES=`ls -1 "${LOCKSDIR}/${SCRIPTNAME_NOARGS}"*.ok 2>/dev/null`
+if [ -n "${OK_FILES}" ]; then
+	touch -d "${RM_OLD_OK_TS}" "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire"
+	echo "${OK_FILES}" | while read FILE; do
+		if [ "${FILE}" -ot "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire" ]; then
+			echo "Removing old ok file: ${FILE}"
+			/usr/bin/logger "Removing old ok file: ${FILE}" 
+			rm "${FILE}"
+		fi
+	done
 fi
