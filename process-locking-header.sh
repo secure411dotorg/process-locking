05fefceb7cad4874ae0359c105a3817c17609ba3
Zigmund Ozea <ziggy@dissectcyber.com>
2014-03-15 17:02:17 -0400

    Fixed bug caused by some command line args and redirected messages previously on STDOUT to STDERR.

--- process-locking-header.sh
+++ process-locking-header.sh
@@ -6,22 +6,22 @@ RM_OLD_OK_TS="48 hours ago"
 # Nothing below here needs configuration
 #
 
-SCRIPTNAME_NOARGS=`echo "$0"|rev|cut -d'/' -f1|rev|sed 's/[^[:alnum:]_.\-]//g'`
+SCRIPTNAME_NOARGS=`echo "${0##*/}"|sed 's/[^[:alnum:]_.\-]//g'`
 
-SCRIPTNAME=`echo "${0}"|rev|cut -d"/" -f1|rev|sed -e "s/\$/.$1.$2.$3/"'; s/[^[:alnum:]._\-]//g; s/\.\.*/./g; s/.$//'`
+SCRIPTNAME=`echo "${SCRIPTNAME_NOARGS}.${1}.${2}.${3}" | sed -e 's/[^[:alnum:]._\-]//g; s/\.\.*/./g; s/.$//'`
 
 if [ -e "${LOCKSDIR}/${SCRIPTNAME}.ok" ];then
-        mv "${LOCKSDIR}/${SCRIPTNAME}.ok" "${LOCKSDIR}/${SCRIPTNAME}.running"
+        mv "${LOCKSDIR}/${SCRIPTNAME}.ok" "${LOCKSDIR}/${SCRIPTNAME}.running" 1>&2
         touch "${LOCKSDIR}/${SCRIPTNAME}.running"
         /usr/bin/logger "Starting $SCRIPTNAME" 
 else   
         if [ -e "${LOCKSDIR}/${SCRIPTNAME}.running" ];then
-                echo "Exiting because process is already running"
+                echo "Exiting because process is already running" 1>&2
                 exit
         else   
                 /usr/bin/logger "Creating ${SCRIPTNAME}.ok"
                 touch "${LOCKSDIR}/${SCRIPTNAME}.ok"
-                echo "Created the ok file - must be first time running."
+                echo "Created the ok file - must be first time running." 1>&2
         	mv "${LOCKSDIR}/$SCRIPTNAME.ok" "${LOCKSDIR}/${SCRIPTNAME}.running"
         	touch "${LOCKSDIR}/${SCRIPTNAME}.running"
         	/usr/bin/logger "Starting ${SCRIPTNAME}" 
@@ -34,8 +34,8 @@ if [ -n "${OK_FILES}" ]; then
 	touch -d "${RM_OLD_OK_TS}" "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire"
 	echo "${OK_FILES}" | while read FILE; do
 		if [ "${FILE}" -ot "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire" ]; then
-			echo "Removing old ok file: ${FILE}"
-			/usr/bin/logger "Removing old ok file: ${FILE}" 
+			echo "Removing old ok file: ${FILE}" 1>&2
+			/usr/bin/logger "Removing old ok file: ${FILE}"
 			rm "${FILE}"
 		fi
 	done
