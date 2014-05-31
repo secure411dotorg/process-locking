34c298f1bee7d3aa5e1a1e229a29309bfe8cac99
Zigmund Ozea <ziggy@dissectcyber.com>
2014-05-31 12:57:51 -0400

    Now records PID of the script to the .running file.

--- process-locking-header.sh
+++ process-locking-header.sh
@@ -12,7 +12,7 @@ SCRIPTNAME=`echo "${SCRIPTNAME_NOARGS}.${1}.${2}.${3}" | sed -e 's/[^[:alnum:]._
 
 if [ -e "${LOCKSDIR}/${SCRIPTNAME}.ok" ];then
         mv "${LOCKSDIR}/${SCRIPTNAME}.ok" "${LOCKSDIR}/${SCRIPTNAME}.running" 1>&2
-        touch "${LOCKSDIR}/${SCRIPTNAME}.running"
+        echo "$$" > "${LOCKSDIR}/${SCRIPTNAME}.running"
         /usr/bin/logger "Starting $SCRIPTNAME" 
 else   
         if [ -e "${LOCKSDIR}/${SCRIPTNAME}.running" ];then
@@ -23,20 +23,20 @@ else
                 touch "${LOCKSDIR}/${SCRIPTNAME}.ok"
                 echo "Created the ok file - must be first time running." 1>&2
         	mv "${LOCKSDIR}/$SCRIPTNAME.ok" "${LOCKSDIR}/${SCRIPTNAME}.running"
-        	touch "${LOCKSDIR}/${SCRIPTNAME}.running"
+        	echo "$$" > "${LOCKSDIR}/${SCRIPTNAME}.running"
         	/usr/bin/logger "Starting ${SCRIPTNAME}" 
         fi
 fi
 
 # Clean up old ok files for files begining with SCRIPTNAME_NOARGS and ending with .ok
-OK_FILES=`ls -1 "${LOCKSDIR}/${SCRIPTNAME_NOARGS}"*.ok 2>/dev/null`
+OK_FILES=`stat -c "%Y %n" "${LOCKSDIR}/${SCRIPTNAME_NOARGS}"*.ok 2>/dev/null`
 if [ -n "${OK_FILES}" ]; then
-	touch -d "${RM_OLD_OK_TS}" "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire"
-	echo "${OK_FILES}" | while read FILE; do
-		if [ "${FILE}" -ot "${LOCKSDIR}/${SCRIPTNAME_NOARGS}.ok-expire" ]; then
+	EXPIRE_EPOCH="$( date -d "${RM_OLD_OK_TS}" +%s )"
+	echo "${OK_FILES}" | while read FILETS FILE; do
+		if [ "${FILETS}" -lt "${EXPIRE_EPOCH:-0}" ]; then
 			echo "Removing old ok file: ${FILE}" 1>&2
 			/usr/bin/logger "Removing old ok file: ${FILE}"
-			rm "${FILE}"
+			rm -f "${FILE}"
 		fi
 	done
 fi
