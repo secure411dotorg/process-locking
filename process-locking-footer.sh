76e8a43c837fb4722b31d4720205166627fe3ee4
Zigmund Ozea <ziggy@dissectcyber.com>
2020-02-26 12:14:37 -0500

    Added a method to set the exit status on unlocked exit

--- process-locking-footer.sh
+++ process-locking-footer.sh
@@ -3,5 +3,8 @@ mv "${LOCKSDIR}/${SCRIPTNAME}.running" "${LOCKSDIR}/${SCRIPTNAME}.ok"
 # Save runtime in seconds and update end of run timestamp
 echo "SECONDS:${SECONDS}" >> "${LOCKSDIR}/${SCRIPTNAME}.ok"
 
+if [ -n "${PLOCK_ESTAT}" ]; then
+	${PLOCKLOG} "Endofrun ${SCRIPTNAME} completed in ${SECONDS} seconds with exit status: ${PLOCK_ESTAT}"
+	exit "${PLOCK_ESTAT}"
+fi
 ${PLOCKLOG} "Endofrun ${SCRIPTNAME} completed in ${SECONDS} seconds"
-
